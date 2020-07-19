28b9a6bb1b15ec04c6e07941be5c2958b0bd5981
setyoawan <setyoawan8@gmail.com>
2020-07-19 19:19:25 +0700

    add indexeddb for save for later article

--- js/nav.js
+++ js/nav.js
@@ -45,6 +45,14 @@ document.addEventListener("DOMContentLoaded", function () {
     xhttp.onreadystatechange = function () {
       if (this.readyState == 4) {
         var content = document.querySelector("#body-content");
+
+        // tambahkan blok if berikut
+        if (page === "home") {
+          getArticles();
+        } else if (page === "saved") {
+          getSavedArticles();
+        }
+        // ---
         if (this.status == 200) {
           content.innerHTML = xhttp.responseText;
         } else if (this.status == 404) {
